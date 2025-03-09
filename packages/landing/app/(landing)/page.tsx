ba02280db3acfd8eebe368501f5cd73bcf633c08
aexshafii <alexandre.shafii@gmail.com>
2025-03-10 00:00:14 +0100

    feat/landing: add bulb to hero

--- packages/landing/app/(landing)/page.tsx
+++ packages/landing/app/(landing)/page.tsx
@@ -50,6 +50,19 @@ export default function Page() {
               height={64}
               className="mx-auto"
             /> */}
+            <div className="flex justify-center">
+              <div className="grid grid-cols-3 gap-0.5">
+                <div className="w-3 h-3"></div>
+                <div className="w-3 h-3 bg-gray-700"></div>
+                <div className="w-3 h-3"></div>
+                <div className="w-3 h-3 bg-gray-700"></div>
+                <div className="w-3 h-3 bg-yellow-300 animate-[pulse_1s_ease-in-out_infinite]"></div>
+                <div className="w-3 h-3 bg-gray-700"></div>
+                <div className="w-3 h-3"></div>
+                <div className="w-3 h-3 bg-gray-700"></div>
+                <div className="w-3 h-3"></div>
+              </div>
+            </div>
           </div>
           <h1 className="text-4xl font-bold tracking-tight sm:text-6xl mb-12">
             <span>Obsidian's </span>
@@ -66,7 +79,7 @@ export default function Page() {
           </div>
           
           <p className="mt-6 text-lg leading-8">
-            Focus on building knowledge, not managing it. Note Companion maintains your vault organized and amplifies your thinking.s
+            Focus on building knowledge, not managing it. Note Companion maintains your vault organized and amplifies your thinking.
             {/* It gives you automatic organization suggestions,  packs a powerful AI chat, and other wizardry such as audio transcription, meeting notes enhancement, and ai image processing (OCR). */}
           </p>
 
