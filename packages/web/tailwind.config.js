759fb35811a2041eca1e276e2ed1a7551e70814d
Benjamin Shafii <ben@prologe.io>
2025-03-14 14:13:58 -0700

    Revert "fix: css in dev mode"
    This reverts commit 40932ccf739025ab772f8317125d94cf769d2aa0.

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -1,10 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    // add more paths if needed
-  ],
   darkMode: ["class"],
   plugins: ["tailwindcss-animate"],
 }
