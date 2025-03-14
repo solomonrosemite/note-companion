40932ccf739025ab772f8317125d94cf769d2aa0
aexshafii <alexandre.shafii@gmail.com>
2025-03-14 21:22:33 +0100

    fix: css in dev mode

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -1,5 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // add more paths if needed
+  ],
   darkMode: ["class"],
   plugins: ["tailwindcss-animate"],
 }
